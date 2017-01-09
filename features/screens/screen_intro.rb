f32aecef0788fe7c32c9b9446b325443827d36b5
Modris Ozols <modris.ozols@testdevlab.com>
2017-01-09 21:54:34 +0200

    Edit screen_intro.rb

--- features/screens/screen_intro.rb
+++ features/screens/screen_intro.rb
@@ -3,9 +3,17 @@ class ScreenIntro < ScreenBase
   def initialize(driver)
     @base = element(:id, 'content')
     @driver = driver
+    @close_intro = element(:id, 'close_intro')
+    @intro_image = element(:id, 'intro_footer_image')
+    @intro_yellow_text = element(:id, 'intro_text_header')
+    @intro_black_text = element(:id, 'intro_text_footer')
   end
 
   def visible?
     @driver.find_element(@base[:type], @base[:value])
+    @driver.find_element(@close_intro[:type], @close_intro[:value])
+    @driver.find_element(@intro_image[:type], @intro_image[:value])
+    @driver.find_element(@intro_yellow_text[:type], @intro_yellow_text[:value])
+    @driver.find_element(@intro_black_text[:type], @intro_black_text[:value])
   end
 end
