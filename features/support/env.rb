6e67535a3446beaa4b76528e871f4a8ccffa6f7c
Modris Ozols <modris.ozols@testdevlab.com>
2017-01-09 21:10:59 +0200

    adding appium.txt, edit device id

--- features/support/env.rb
+++ features/support/env.rb
@@ -18,7 +18,7 @@ end
 options = {
   'port' => 5500,
   'portboot' => 5051,
-  'sn' => '10.1.5.148:5555',
+  'sn' => 'LGD855200400a3',
   'app' => 'app.apk',
   'appPackage' => 'com.testdevlab.notifyus'
 
